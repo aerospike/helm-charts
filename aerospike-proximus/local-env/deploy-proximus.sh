cde672fc7650396bf7b4ee1fcdde055d250cb7a2
davi17g <dgerchikov@aerospike.com>
2024-03-27 12:25:16 +0200

    Enable nginx

--- aerospike-proximus/local-env/deploy-proximus.sh
+++ aerospike-proximus/local-env/deploy-proximus.sh
@@ -2,4 +2,4 @@
 WORKSPACE="$(git rev-parse --show-toplevel)"
 kubectl --namespace aerospike create secret generic aerospike-proximus-secret --from-file=features.conf="$WORKSPACE/aerospike-proximus/local-env/secrets/features.conf"
 helm install proximus "$WORKSPACE/aerospike-proximus" -f "$WORKSPACE/aerospike-proximus/local-env/config/values.yaml" -n aerospike
-kubectl apply -f "$WORKSPACE/aerospike-proximus/local-env/config/aerospike-proximus-ingress.yaml"
+#kubectl apply -f "$WORKSPACE/aerospike-proximus/local-env/config/aerospike-proximus-ingress.yaml"
--- aerospike-proximus/local-env/run-quote-search.sh
+++ aerospike-proximus/local-env/run-quote-search.sh
@@ -21,13 +21,20 @@ if ! docker images --format "{{.Repository}}" | grep -q "^quote-search"; then
 #-e "VERIFY_TLS=False" \
 #-e "GRPC_DNS_RESOLVER=native" quote-search
 
+#docker run -d \
+#--name "quote-search" \
+#-v "$WORKSPACE/aerospike-proximus/proximus-examples/quote-semantic-search/container-volumes/quote-search/data:/container-volumes/quote-search/data" \
+#--network "kind" -p "8080:8080" \
+#-e "PROXIMUS_HOST=$(kubectl -n aerospike get svc/aerospike-proximus-ingress -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')" \
+#-e "PROXIMUS_PORT=5000" \
+#-e "APP_NUM_QUOTES=5000" \
+#-e "GRPC_DNS_RESOLVER=native" quote-search
+
 docker run -d \
 --name "quote-search" \
 -v "$WORKSPACE/aerospike-proximus/proximus-examples/quote-semantic-search/container-volumes/quote-search/data:/container-volumes/quote-search/data" \
---network "kind" -p "8080:8080" \
--e "PROXIMUS_HOST=$(kubectl -n aerospike get svc/aerospike-proximus-ingress -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')" \
+--net "host" -p "8080:8080" \
+-e "PROXIMUS_HOST=host.docker.internal" \
 -e "PROXIMUS_PORT=5000" \
 -e "APP_NUM_QUOTES=5000" \
--e "VERIFY_TLS=False" \
 -e "GRPC_DNS_RESOLVER=native" quote-search
-
