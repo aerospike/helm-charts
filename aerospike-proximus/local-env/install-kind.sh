99f966faf76c339caf6ee5df06623a4a645d9d93
David Gerchikov <dgerchikov@aerospike.com>
2024-05-03 12:48:50 +0300

    Add helm charts for quote-search Proximus app

--- aerospike-proximus/local-env/install-kind.sh
+++ aerospike-proximus/local-env/install-kind.sh
@@ -1,6 +1,15 @@
 #!/bin/bash -e
 WORKSPACE="$(git rev-parse --show-toplevel)"
 
+# Check prerequisites
+REQUISITES=("kubectl" "kind" "docker" "helm")
+for item in "${REQUISITES[@]}"; do
+  if [[ -z $(which "${item}") ]]; then
+    echo "${item} cannot be found on your system, please install ${item}"
+    exit 1
+  fi
+done
+
 if [ ! -f "$WORKSPACE/aerospike-proximus/local-env/config/features.conf" ]; then
   echo "features.conf Not found"
   exit 1
@@ -49,15 +58,19 @@ while true; do
   fi
 done
 
-echo "Deploy MetalLB"
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.4/config/manifests/metallb-native.yaml
-kubectl wait --namespace metallb-system \
-                --for=condition=ready pod \
-                --selector=app=metallb \
-                --timeout=90s
-kubectl apply -f "$WORKSPACE/aerospike-proximus/local-env/config/metallb-config.yaml"
+echo "Deploy Nginx"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
+
 
 sleep 30
 echo "Deploy Proximus"
 helm install as-quote-search "$WORKSPACE/aerospike-proximus" \
 --values "$WORKSPACE/aerospike-proximus/examples/quote-search/as-quote-search-values.yaml" --namespace aerospike
+
+echo "Deploying Quote-Search"
+helm install quote-semantic-search "$WORKSPACE/aerospike-proximus-examples/quote-semantic-search" \
+--values "$WORKSPACE/aerospike-proximus/local-env/config/quote-semantic-search-values.yaml" --namespace aerospike --wait
