342cd78531e343fca67399b39bc6b40aeb5f2fb4
David Gerchikov <dgerchikov@aerospike.com>
2024-05-22 17:43:49 +0300

    Update uninstall-kind.sh

--- aerospike-vector-search/kind/uninstall-kind.sh
+++ aerospike-vector-search/kind/uninstall-kind.sh
@@ -2,18 +2,18 @@
 
 WORKSPACE="$(git rev-parse --show-toplevel)"
 
-helm uninstall quote-search --namespace aerospike
-helm uninstall avs --namespace aerospike
-kubectl delete -f "$WORKSPACE/aerospike-proximus/kind/config/virtual-service-vector-search.yaml"
-kubectl delete -f "$WORKSPACE/aerospike-proximus/kind/config/gateway.yaml"
+#helm uninstall quote-search --namespace aerospike
+#helm uninstall avs --namespace aerospike
+kubectl delete -f "$WORKSPACE/aerospike-vector-search/kind/config/virtual-service-vector-search.yaml"
+kubectl delete -f "$WORKSPACE/aerospike-vector-search/kind/config/gateway.yaml"
 helm uninstall istio-ingress --namespace istio-ingress
 kubectl delete namespace istio-ingress
 helm uninstall istiod --namespace istio-system
 helm uninstall istio-base --namespace istio-system
 kubectl delete namespace istio-system
-kubectl delete -f "$WORKSPACE/aerospike-proximus/kind/config/metallb-config.yaml"
+kubectl delete -f "$WORKSPACE/aerospike-vector-search/kind/config/metallb-config.yaml"
 kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/v0.14.4/config/manifests/metallb-native.yaml
-kubectl delete -f "$WORKSPACE/aerospike-proximus/examples/kind/aerospike.yaml"
+kubectl delete -f "$WORKSPACE/aerospike-vector-search/examples/kind/aerospike.yaml"
 kubectl --namespace aerospike delete secret auth-secret
 kubectl --namespace aerospike delete secret aerospike-secret
 kubectl delete clusterrolebinding aerospike-cluster
