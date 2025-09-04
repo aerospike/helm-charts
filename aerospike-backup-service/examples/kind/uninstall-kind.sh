9e3baaf9c0a9ef8c173c7f4b270a23fee9b1bb48
David Gerchikov <dgerchikov@aerospike.com>
2025-09-04 10:14:50 +0300

    Remove Aerospike Backup Service Helm chart

--- aerospike-backup-service/examples/kind/uninstall-kind.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-WORKSPACE="$(git rev-parse --show-toplevel)"
-
-helm uninstall abs --namespace aerospike
-kubectl delete secret --namespace aerospike psw-cm
-kubectl delete secret --namespace aerospike credentials
-kubectl delete namespace aerospike
-docker compose --file "$WORKSPACE/aerospike-backup-service/examples/kind/docker-compose.yaml" down
-kind delete cluster
