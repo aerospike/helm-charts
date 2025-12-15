17897b3eda65bb9efc14089ee32289eb0dc6c19d
Vivek <vbayana@aerospike.com>
2025-12-15 17:26:48 +0530

    Added esp-outbound and xdr-proxy into all connectors tests

--- ci/connectors/run-all-connector-tests.sh
+++ ci/connectors/run-all-connector-tests.sh
@@ -62,12 +62,12 @@ fi
 
 # Connectors to test
 CONNECTORS=(
-#    "aerospike-esp-outbound"
+    "aerospike-esp-outbound"
     "aerospike-jms-inbound"
     "aerospike-jms-outbound"
     "aerospike-kafka-outbound"
     "aerospike-pulsar-outbound"
-#    "aerospike-xdr-proxy"
+    "aerospike-xdr-proxy"
 )
 
 # Results tracking (using indexed arrays for bash 3.2 compatibility)
