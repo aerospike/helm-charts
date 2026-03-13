0f85db75473b2971e002bec388f57e4a036a113d
mphani <pmokrala@aerospike.com>
2026-03-13 23:49:37 +0530

    CONNECTOR-1370

--- aerospike-elasticsearch-outbound/tests/integration-test/run-integration-test.sh
+++ aerospike-elasticsearch-outbound/tests/integration-test/run-integration-test.sh
@@ -180,10 +180,6 @@ print_info "Step 3: Deploying ElasticSearch Outbound connector..."
 
 print_info "helm install ${ES_RELEASE} $SCRIPT_DIR/../.. -n ${NAMESPACE} -f $SCRIPT_DIR/elastic-outbound-integration-values.yaml --wait --timeout=2m"
 
-print_info "✅ ElasticSearch Outbound deployed ... sleeping for 300 seconds"
-
-sleep 300
-
 helm install "${ES_RELEASE}" "$SCRIPT_DIR/../.." \
   -n "${NAMESPACE}" -f "$SCRIPT_DIR/elastic-outbound-integration-values.yaml" --wait --timeout=2m
 print_info "✅ ElasticSearch Outbound deployed"
