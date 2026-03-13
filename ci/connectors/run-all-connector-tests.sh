1b5647bd9c4d900dbfd5237b5e43078149cbd770
mphani <pmokrala@aerospike.com>
2026-03-14 01:04:15 +0530

    CONNECTOR-1370

--- ci/connectors/run-all-connector-tests.sh
+++ ci/connectors/run-all-connector-tests.sh
@@ -63,10 +63,10 @@ fi
 # Connectors to test
 CONNECTORS=(
     "aerospike-esp-outbound"
-    # "aerospike-jms-inbound"
-    # "aerospike-jms-outbound"
-    # "aerospike-kafka-outbound"
-    # "aerospike-pulsar-outbound"
+    "aerospike-jms-inbound"
+    "aerospike-jms-outbound"
+    "aerospike-kafka-outbound"
+    "aerospike-pulsar-outbound"
     "aerospike-xdr-proxy"
     "aerospike-elasticsearch-outbound"
 )
